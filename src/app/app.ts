ff7f329f036eaff11d716bca39ecc21a08f8a240
Holly Henske <hcover333@gmail.com>
2025-12-31 15:33:38 -0600

    top section roughed in, working on game-board size

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { GameBoardComponent } from './components/board/game-board/game-board.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [GameBoardComponent],
+  imports: [GameBoardComponent, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   
