843c35f66de83a25ff2c61917d289d2c1fb323a5
Holly Henske <hcover333@gmail.com>
2026-01-02 11:06:16 -0600

    score card section roughed in, as well as player-indicator section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { GameBoardComponent } from './components/board/game-board/game-board.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PlayerIndicatorComponent } from "./components/board/player-indicator/player-indicator.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [GameBoardComponent, HeaderComponent],
+  imports: [GameBoardComponent, HeaderComponent, PlayerIndicatorComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
